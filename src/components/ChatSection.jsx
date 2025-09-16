b9a0cc896613874e243cdb2c321345781b943b1d
The-vishnu <143195086+The-vishnu@users.noreply.github.com>
2025-09-16 11:10:39 +0530

    comite

---
+++ src/components/ChatSection.jsx
@@ -0,0 +1,142 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Send, User, Clock, PhoneCall, Video, Info} from "lucide-react";
+
+function ChatSection() {
+  const date = new Date();
+  const showTime = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const [SenderMessages, setSenderMessages] = useState([]);
+  const [RecevierMessaeg, setRecevierMessage] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+  const [isActive, setActiveStatus] = useState(true);
+  const messagesEndRef = useRef(null);
+
+  const sendMessageSound = new Audio("/assets/notification5.mp3");
+
+  const handleSend = () => {
+    if (inputMessage.trim() !== "") {
+      sendMessageSound.play();
+      setSenderMessages([
+        ...SenderMessages,
+        {
+          text: inputMessage,
+          timeStamp: showTime,
+          senderId: "You",
+        },
+      ]);
+      setInputMessage("");
+    }
+  };
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [SenderMessages, RecevierMessaeg]);
+  return (
+    <>
+      <div className="w-full bg-gray-200 h-screen flex flex-col justify-between rounded-3xl p-4 ">
+        {/* 🌸 User Profile Section */}
+        <div className="flex items-center justify-between bg-gray-100 p-2 rounded-2xl">
+          {/* Profile Info */}
+          <div className="flex items-center space-x-4">
+            <img
+              src="https://i.pravatar.cc/50"
+              alt="User Profile"
+              className="w-12 h-12 rounded-full"
+            />
+
+            <div>
+              <p class="text-lg  group relative w-max cursor-pointer">
+                <span>Arvind</span>
+                <span class="absolute -bottom-1 right-0 w-0 transition-all h-0.5 bg-gray-400 group-hover:w-full"></span>
+              </p>
+              <span
+                className={`text-sm ${
+                  isActive ? "text-green-400" : "text-gray-400"
+                }`}
+              >
+                {isActive ? "online" : "offline"}
+              </span>{" "}
+              {/* Green for Online, Gray for Offline */}
+            </div>
+          </div>
+
+          {/* Call Buttons */}
+          <div className="flex space-x-3">
+            <button className="bg-gray-300 hover:bg-slate-400 p-2 rounded-full cursor-pointer">
+              <PhoneCall size={20}/> {/* Call Icon */}
+            </button>
+
+            <button className="bg-gray-300 hover:bg-slate-400 p-2 rounded-full cursor-pointer">
+              <Video size={20}/> {/* Video Call Icon */}
+            </button>
+
+             <button className=" hover:bg-slate-400 p-2 h-10 items-center justify-center w-10 rounded-full cursor-pointer flex flex-col gap-0.5">
+              <span className="w-1 h-1 bg-gray-800 rounded-full"></span>
+              <span className="w-1 h-1 bg-gray-800 rounded-full"></span>
+              <span className="w-1 h-1 bg-gray-800 rounded-full"></span>
+            </button>
+          </div>
+        </div>
+        {/* Messages Area */}
+        <div className="flex flex-col space-y-4 overflow-auto flex-1 mb-4 hide-scrollbar">
+          {/* Sent Message (User → Bot) */}
+          {SenderMessages.map((sendermsg, map) => (
+            <div className="flex flex-col items-end">
+              {/* Username + Time */}
+              <div className="flex items-center space-x-2 text-sm text-gray-600">
+                <span className="font-semibold">{sendermsg.senderId}</span>
+                <span>{sendermsg.timeStamp}</span>
+              </div>
+
+              {/* Message Bubble */}
+              <div className="bg-gray-300 p-3 rounded-3xl rounded-br-none max-w-xs break-words">
+                {sendermsg.text}
+              </div>
+            </div>
+          ))}
+
+          {/* Received Message (Bot → User) */}
+          {RecevierMessaeg.map((RecevierMessaeg, map) => (
+            <div className="flex flex-col items-end">
+              {/* Username + Time */}
+              <div className="flex items-center space-x-2 text-sm text-gray-600">
+                <span className="font-semibold">Arvind</span>
+                <span>10:45 AM</span>
+              </div>
+
+              {/* Message Bubble */}
+              <div className="bg-gray-300 p-3 rounded-3xl rounded-bl-none max-w-xs break-words">
+                {RecevierMessaeg}
+              </div>
+            </div>
+          ))}
+          <div ref={messagesEndRef}></div>
+        </div>
+
+        {/* Input Section */}
+        <div className="p-3 bg-gray-100 rounded-3xl flex items-center space-x-3">
+          {/* Input Field */}
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            className="flex-1 p-3 rounded-full focus:outline-none bg-white shadow"
+          />
+
+          {/* Send Button with Icon */}
+          <button
+            className="hover:bg-slate-400 bg-gray-300 p-3 rounded-full cursor-pointer"
+            onClick={handleSend}
+          >
+            <Send size={20} />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ChatSection;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,94 @@
+import { React, useState } from "react";
+import { Search, Settings, Users, Info, Cpu, Inbox } from "lucide-react";
+
+function Sidebar() {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <>
+      <div className="h-screen w-[60px] bg-gray-200 flex flex-col items-center py-6 gap-6 rounded-r-3xl shadow-lg relative">
+        {/* Icon Buttons with Tooltip */}
+        <div className="flex flex-col gap-6 relative">
+          {/* Home */}
+          <div className="relative group">
+            <Inbox
+              size={35}
+              className="cursor-pointer p-2 rounded-lg hover:bg-gray-300 transition-all duration-200"
+            />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-3 bg-gray-300  text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+              Inbox
+            </span>
+          </div>
+          {/* Home */}
+          <div className="relative group">
+            <Cpu
+              size={35}
+              className="cursor-pointer p-2 rounded-lg hover:bg-gray-300 transition-all duration-200"
+            />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-3 bg-gray-300 text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+              Ais
+            </span>
+          </div>
+
+          {/* Friends / Users */}
+          <div className="relative group">
+            <Users
+              size={35}
+              className="cursor-pointer p-2 rounded-lg hover:bg-gray-300 transition-all duration-200"
+            />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-3 bg-gray-300 text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+              Friends
+            </span>
+          </div>
+
+          {/* Settings */}
+          <div className="relative group w-[15px]">
+            <Settings
+              size={35}
+              className="cursor-pointer p-2 rounded-lg hover:bg-gray-300 transition-all duration-200"
+            />
+            <span className="absolute left-full top-1/2 -translate-y-1/2 ml-3 bg-gray-300 text-sm px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap">
+              Settings
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-100  flex flex-col transition-all duration-300 w-120 p-1 m-1 justify-between rounded-3xl">
+        {/* Search section */}
+        <div className="w-90 h-12 bg-gray-200 justify-between items-center p-2 m-2 flex flex-row gap-2 rounded-2xl">
+          <input
+            type="text"
+            placeholder="example@gmail.com"
+            className="w-80 h-8 rounded-full focus:outline-none bg-gray-70 px-4 py-2 focus:ring-2 focus:ring-gray-400"
+          />
+          <Search className="cursor-pointer" />
+        </div>
+        {/* friends Profile section */}
+        <div className=" w-full min-h-[500px]">
+          <div className="flex flex-row justify-between items-center w-full min-h-[50px] border-b-0.1 p-0.5 m-0.5    hover:bg-gray-200 transition-all  rounded-lg mb-2 cursor-pointer">
+            <div className="flex flex-row gap-2 items-center">
+              <img
+                src="https://i.pravatar.cc/50"
+                alt="User Profile"
+                className="w-12 h-12 rounded-full"
+              />
+              <span>Sam Altman</span>
+            </div>
+            <span>10:40 PM</span>
+          </div>
+        </div>
+        {/* User section */}
+        <div className="flex flex-row gap-2 items-center m-1 p-1">
+          <img
+            src="https://i.pravatar.cc/50"
+            alt="User Profile"
+            className="w-8 h-8 rounded-full"
+          />
+          <span className="text-[15px] text-sm">Arvind Shrivastav</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
